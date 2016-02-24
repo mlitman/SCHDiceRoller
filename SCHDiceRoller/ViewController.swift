200b10a4f4c3be26c2aea10b633ae44d7fd753f4
Mike Litman <awesomefat@gmail.com>
2016-02-24 14:02:02 -0600

    session: 2/24/16

--- SCHDiceRoller/ViewController.swift
+++ SCHDiceRoller/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func page2ButtonPressed(sender: AnyObject)
+    {
+        let vc = self.storyboard?.instantiateViewControllerWithIdentifier("Page2VC") as! Page2VC
+        vc.theText = self.theTextField.text!
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,7 +31,8 @@ class ViewController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
     {
         let vc = segue.destinationViewController as! Page2VC
-        vc.theText = "I told you so!!!"
+        //vc.theText = "I told you so!!!"
+        vc.theText = self.theTextField.text!
     }
 }
 
